2f1943483bef173b257c94ab92c8072e5a023839
Tommm10 <tommccorquodale@gmail.com>
2017-06-14 23:47:57 -0500

    Configured main view

---
+++ A2BNOW/GradientView.swift
@@ -0,0 +1,30 @@
+//
+//  GradientView.swift
+//  A2BNOW
+//
+//  Created by Thomas McCorquodale on 6/14/17.
+//  Copyright © 2017 Tom McCorquodale. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    let gradient = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        setupGradientView()
+    }
+    
+    func setupGradientView() {
+        gradient.frame = self.bounds
+        gradient.colors = [UIColor.white.cgColor, UIColor.init(white: 1.0, alpha: 0.0).cgColor]
+        gradient.startPoint = CGPoint.zero
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        gradient.locations = [0.8, 1.0]
+        self.layer.addSublayer(gradient)
+        
+        
+    }
+
+}
--- A2BNOW/ViewController.swift
+++ A2BNOW/ViewController.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    @IBOutlet var mapKit: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapKit.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
